19e0768dcde15684095eca03f4495eadf953ae42
Matheus Farias <matheusfarias009@hotmail.com>
2020-10-19 10:58:00 -0300

    problem solved

--- 06_not_bad.py
+++ 06_not_bad.py
@@ -11,7 +11,11 @@ Exemplo: 'The dinner is not that bad!' retorna 'The dinner is good!'
 
 def not_bad(s):
     # +++ SUA SOLUÇÃO +++
-    return
+    _not = s.find('not')
+    _bad = s.find('bad')
+    if _not < _bad:
+        s = s.replace(s[_not:_bad+3], 'good')
+    return s
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
